09254f094a3ec0c9d7fca1be7325ab99fdd6c387
Austin <joha0033@gmail.com>
2017-06-17 11:53:44 -0600

    right side nav bar working, needs email population

--- app.js
+++ app.js
@@ -29,3 +29,14 @@ function showSurfers(surfers) {
     console.log('hella what. ' + text);
     //alert( "Handler for .click() called." );
   })
+
+  // Initialize collapse button
+ $(".button-collapse").sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    });
+ // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+ $('.collapsible').collapsible();
+ // Show sideNav
